7417f7c299d3c4b316716275fbec260ddcc1e4df
Thomas Farstrike <thomasverstreken@protonmail.com>
2026-01-13 19:24:29 +0100

    Add missing resolutions

--- driver/sensor.c
+++ driver/sensor.c
@@ -33,11 +33,18 @@ const resolution_info_t resolution[FRAMESIZE_INVALID] = {
     {  320,  320, ASPECT_RATIO_1X1   }, /* 320x320 */
     {  400,  296, ASPECT_RATIO_4X3   }, /* CIF   */
     {  480,  320, ASPECT_RATIO_3X2   }, /* HVGA  */
+    {  480,  480, ASPECT_RATIO_1X1   }, /* 480x480 */
     {  640,  480, ASPECT_RATIO_4X3   }, /* VGA   */
+    {  640,  640, ASPECT_RATIO_1X1   }, /* 640x640 */
+    {  720,  720, ASPECT_RATIO_1X1   }, /* 720x720 */
     {  800,  600, ASPECT_RATIO_4X3   }, /* SVGA  */
+    {  800,  800, ASPECT_RATIO_1X1   }, /* 800x800 */
+    {  900,  900, ASPECT_RATIO_1X1   }, /* 900x900 */
     { 1024,  768, ASPECT_RATIO_4X3   }, /* XGA   */
     { 1280,  720, ASPECT_RATIO_16X9  }, /* HD    */
+    { 1024, 1024, ASPECT_RATIO_1X1   }, /* 1024x1024 */
     { 1280, 1024, ASPECT_RATIO_5X4   }, /* SXGA  */
+    { 1280, 1280, ASPECT_RATIO_1X1   }, /* 1280x1280 */
     { 1600, 1200, ASPECT_RATIO_4X3   }, /* UXGA  */
     // 3MP Sensors
     { 1920, 1080, ASPECT_RATIO_16X9  }, /* FHD   */
